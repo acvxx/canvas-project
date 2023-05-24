d2e7a012a20d5191ceb61b13c3e49f4145b68540
GyeongAh Kang <acvxxx@korea.ac.kr>
2023-05-24 10:13:20 +0900

    Feat: shape move

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,5 +1,13 @@
 import React, { useRef, useState } from "react";
-import { Stage, Layer, Rect, Circle, Transformer, Line } from "react-konva";
+import {
+  Stage,
+  Layer,
+  Rect,
+  Circle,
+  Transformer,
+  Line,
+  Ellipse,
+} from "react-konva";
 
 const Canvas = () => {
   const stageRef = useRef();
@@ -8,36 +16,62 @@ const Canvas = () => {
   const [isDrawing, setIsDrawing] = useState(false);
   const [startPoint, setStartPoint] = useState({ x: 0, y: 0 });
   const [endPoint, setEndPoint] = useState({ x: 0, y: 0 });
+  const [color, setColor] = useState("white");
   const [shapeType, setShapeType] = useState("");
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
 
   const handleMouseDown = (e) => {
-    if (!isDrawing) return;
     const { offsetX, offsetY } = e.evt;
     setStartPoint({ x: offsetX, y: offsetY });
   };
 
   const handleMouseMove = (e) => {
-    if (!isDrawing) return;
     const { offsetX, offsetY } = e.evt;
     setEndPoint({ x: offsetX, y: offsetY });
   };
 
   const handleMouseUp = () => {
-    if (!isDrawing) return;
-    //setIsDrawing(false);
+    console.log(selectedShapeId);
+    if (!isDrawing && selectedShapeId !== null) {
+      console.log(isDrawing, selectedShapeId);
+      const x = endPoint.x - startPoint.x;
+      const y = endPoint.y - startPoint.y;
+      const shape1 = shapes[selectedShapeId];
+
+      const updatedShape = {
+        type: shape1.type,
+        start: { x: shape1.start.x + x, y: shape1.start.y + y },
+        end: { x: shape1.end.x + x, y: shape1.end.y + y },
+        fill: "red",
+        stroke: "black",
+      };
+      updateShape(selectedShapeId, updatedShape);
+      return;
+    } else if (!isDrawing) return;
+    setIsDrawing(false);
     const shape = {
       type: shapeType, // 원하는 도형 타입 설정
       start: { ...startPoint },
       end: { ...endPoint },
+      fill: color,
+      stroke: "black",
     };
     setShapes([...shapes, shape]);
     setStartPoint({ x: 0, y: 0 });
     setEndPoint({ x: 0, y: 0 });
   };
 
-  const handleShapeClick = (event, id) => {
+  const handleShapeClick = (event, id, shape) => {
     event.cancelBubble = true;
     setSelectedShapeId(id);
+    const updatedShape = {
+      type: shape.type,
+      start: { x: shape.start.x, y: shape.start.y },
+      end: { x: shape.end.x, y: shape.end.y },
+      fill: "blue",
+      stroke: "black",
+    };
+    updateShape(id, updatedShape);
   };
 
   const handleStageClick = () => {
@@ -51,17 +85,75 @@ const Canvas = () => {
     setSelectedShapeId(null);
   };
 
+  const updateShape = (index, updatedShape) => {
+    setShapes((prevShapes) => {
+      const updatedShapes = [...prevShapes];
+      updatedShapes[index] = updatedShape;
+      return updatedShapes;
+    });
+  };
+
   return (
     <div>
-      <button onClick={() => setIsDrawing(!isDrawing)}>
-        {isDrawing ? "Stop Drawing" : "Start Drawing"}
+      <h6>
+        {isDrawing ? "그림을 그리고 있어요" : "그림을 그리지 않고 있어요."}
+      </h6>
+      <h2>
+        {selectedShapeId == null
+          ? "선택된 도형이 없어요."
+          : JSON.stringify(shapes[selectedShapeId])}
+      </h2>
+      <br></br>
+      <button
+        onClick={() => {
+          setShapeType("rectangle");
+          setIsDrawing(true);
+          setColor("white");
+        }}
+      >
+        사각형
       </button>
-      <button onClick={() => setShapeType("rectangle")}>사각형</button>
-      <button onClick={() => setShapeType("circle")}>원</button>
+      <button
+        onClick={() => {
+          setShapeType("circle");
+          setIsDrawing(true);
+          setColor("white");
+        }}
+      >
+        원
+      </button>
+      <button
+        onClick={() => {
+          setShapeType("ellipse");
+          setIsDrawing(true);
+          setColor("white");
+        }}
+      >
+        타원
+      </button>
+      <button
+        onClick={() => {
+          setShapeType("line");
+          setIsDrawing(true);
+          setColor("white");
+        }}
+      >
+        직선
+      </button>
+      <button
+        onClick={() => {
+          setShapeType("");
+          setIsDrawing(false);
+          setColor("white");
+        }}
+      >
+        도형 선택하기
+      </button>
       <Stage
         width={window.innerWidth}
         height={window.innerHeight}
         ref={stageRef}
+        stroke={"Black"}
         onMouseDown={handleMouseDown}
         onMouseMove={handleMouseMove}
         onMouseUp={handleMouseUp}
@@ -69,11 +161,12 @@ const Canvas = () => {
       >
         <Layer>
           {shapes.map((shape, index) => {
+            const { start, end } = shape;
+            const width = Math.abs(end.x - start.x);
+            const height = Math.abs(end.y - start.y);
+            const fill = shape.fill;
+            const radius = Math.abs(end.x - start.x) / 2;
             if (shape.type === "rectangle") {
-              const { start, end } = shape;
-              const width = end.x - start.x;
-              const height = end.y - start.y;
-
               return (
                 <Rect
                   key={index}
@@ -81,28 +174,63 @@ const Canvas = () => {
                   y={start.y}
                   width={width}
                   height={height}
-                  fill={index === selectedShapeId ? "blue" : "red"}
-                  onClick={(event) => handleShapeClick(event, index)}
-                  draggable
+                  stroke={"black"}
+                  fill={fill}
+                  onClick={(event) => {
+                    handleShapeClick(event, index, shape);
+                  }}
+                  //onUpdate={(updatedShape) => updateShape(index, updatedShape)}
+                  //draggable
                 />
               );
             }
             if (shape.type === "circle") {
-              const { start, end } = shape;
-              const radius = Math.abs(end.x - start.x) / 2;
               return (
                 <Circle
                   key={index}
                   x={(start.x + end.x) / 2}
                   y={(start.y + end.y) / 2}
                   radius={radius}
-                  fill={index === selectedShapeId ? "blue" : "red"}
-                  onClick={(event) => handleShapeClick(event, index)}
-                  draggable
+                  stroke={"black"}
+                  fill={fill}
+                  onClick={(event) => {
+                    handleShapeClick(event, index, shape);
+                  }}
+                  //draggable
                 />
               );
             }
-
+            if (shape.type === "ellipse") {
+              return (
+                <Ellipse
+                  key={index}
+                  x={(start.x + end.x) / 2}
+                  y={(start.y + end.y) / 2}
+                  radiusX={width / 2}
+                  radiusY={height / 2}
+                  stroke={"black"}
+                  fill={fill}
+                  onClick={(event) => {
+                    handleShapeClick(event, index, shape);
+                  }}
+                  //draggable
+                />
+              );
+            }
+            if (shape.type === "line") {
+              return (
+                <Line
+                  key={index}
+                  points={[start.x, start.y, end.x, end.y]}
+                  stroke={"black"}
+                  fill={fill}
+                  onClick={(event) => {
+                    handleShapeClick(event, index, shape);
+                  }}
+                  //draggable
+                />
+              );
+            }
             return null;
           })}
         </Layer>
--- src/components/Shape.js
+++ src/components/Shape.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Rect, Circle } from "react-konva";
 
 const Shape = ({ shape, isSelected, onSelect, onDeselect }) => {
